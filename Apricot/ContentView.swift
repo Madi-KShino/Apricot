b5540917294c6672da7af2de4913b1c141cbd747
mshino <mshino@hh2.com>
2020-08-05 15:10:09 -0600

    Setup and pod install

---
+++ Apricot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Apricot
+//
+//  Created by Madison Shino on 8/5/20.
+//  Copyright © 2020 madiShino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
